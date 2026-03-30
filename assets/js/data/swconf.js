fddad74c83043b691aa38ea72d778d5a4a738f92
0xasifshaik <0xasifshaik@users.noreply.github.com>
2026-03-30 16:44:21 +0000

    Deploy from Private Source 0xasifshaik/pikachu-blog@570cc7b14b236d9a13170098b4df8072f45fbd27

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774888637',resources: [
+    cacheName: 'chirpy-1774889060',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
